6952bff521cc75daa41c3e28c1bd39db8e283bee
Àngel <angel@rom.cat>
2022-10-21 18:53:03 +0200

    Agregar archivos de proyecto.

---
+++ 04_Data/ViewModels/ClientesEmpleadosNavierasPedidoViewModel.cs
@@ -0,0 +1,15 @@
+﻿using _04_Data.Data;
+using _04_Data.Dtos;
+using System.Collections.Generic;
+
+namespace _04_Data.ViewModels
+{
+    public class ClientesEmpleadosNavierasPedidoViewModel
+    {
+        //public IList<Cliente> clientes { get; set; }
+        public IList<ClienteDto> clienteDtos { get; set; }
+        public IList<Empleado> empleados { get; set; }
+        public IList<Naviera> navieras { get; set; }
+        public Pedido pedido { get; set; }
+    }
+}
